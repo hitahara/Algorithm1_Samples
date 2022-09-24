732ced6fe0d620071d05ca0f1817723ec1dad54e
Yuki Nishidate <asta18425@gmail.com>
2022-09-24 18:41:13 +0900

    refactor stack

--- 04/stack_array.c
+++ 04/stack_array.c
@@ -2,43 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-/** @brief スタックの配列がとれる最大要素数. */
 #define SIZE 1000
 
-/**
- * @brief スタックの配列とメタ情報を保持する構造体.
- */
 typedef struct {
-  int length;         /** 配列の長さ. */
-  int elements[SIZE]; /** 要素の配列. */
+  int length;
+  int elements[SIZE];
 } stack;
 
-/**
- * @brief stackの初期化.
- */
-stack* init_stack() {
-  stack* s = (stack*)malloc(sizeof(stack));
-  s->length = 0;
-  return s;
-}
-
-/**
- * @brief stackの要素をクリアする.
- */
 void clear_stack(stack* stack) {
   stack->length = 0;
 }
 
-/**
- * @brief stackのメモリを解放.
- */
-void release_stack(stack* stack) {
-  free(stack);
-}
-
-/**
- * @brief valをスタックにプッシュする.
- */
 void push(stack* stack, int val) {
   if (stack->length >= SIZE) {
     printf("No more element can be pushed into the stack.\n");
@@ -49,9 +23,6 @@ void push(stack* stack, int val) {
   stack->length++;
 }
 
-/**
- * @brief スタックの先頭をポップする.
- */
 void pop(stack* stack, int* val) {
   if (stack->length == 0) {
     printf("No more element can be popped from the elements.\n");
@@ -62,35 +33,33 @@ void pop(stack* stack, int* val) {
   *val = stack->elements[stack->length];
 }
 
-/**
- * @brief stack確認用プリント関数.
- */
 void print_stack(stack* s) {
   printf("ELEMENTS: [ ");
   for (int i = s->length - 1; i >= 0; i--) {
     printf("%d ", s->elements[i]);
   }
-  printf("]\nSTACK POINTER: %ld\n", s->length);
+  printf("]\n");
+  printf("STACK POINTER: %d\n", s->length);
 }
 
 int main() {
-  stack* s = init_stack();
-  print_stack(s);
+  stack s;
+  s.length = 0;
+  print_stack(&s);
 
   for (int i = 0; i < 10; i++) {
-    push(s, i);
+    push(&s, i);
   }
-  print_stack(s);
+  print_stack(&s);
 
   int val;
-  pop(s, &val);
-  print_stack(s);
+  pop(&s, &val);
+  print_stack(&s);
   printf("POP: %d\n", val);
 
-  clear_stack(s);
-  print_stack(s);
+  clear_stack(&s);
+  print_stack(&s);
 
-  release_stack(s);
   return 0;
 }
 
