d2a52364b9489befcf6749d76c99c52cc50290cd
Yuki Nishidate <asta18425@gmail.com>
2022-09-25 11:48:45 +0900

    add notes

--- 03/array.c
+++ 03/array.c
@@ -3,11 +3,29 @@
 
 #define SIZE 1000
 
+// NOTE:
+// C 言語では、struct name {}; と定義してしまうと変数宣言の
+// 際に毎回 struct name var; と書く必要があります。
+// typedef struct {} name; と書くことで name var; と簡潔に
+// 書くことができます。
 typedef struct {
   int length;
   int elements[SIZE];
 } sequence;
 
+// NOTE:
+// 2 つの理由から関数には構造体のポインタを渡しています。
+// - 1 つめの理由は関数内で構造体を変更するためです。
+//   構造体自体を渡してしまうと、構造体のコピーが発生するため
+//   関数呼び出し側が渡したインスタンスに影響を与えられません。
+// - 2 つめの理由はパフォーマンスを改善するためです。
+//   構造体自体を渡してしまうと、構造体のコピーが発生するため
+//   低速です。CPU の演算速度に比べるとメモリアクセスは超低速です
+//   ので注意が必要です。ポインタを渡すと構造体のコピーが発生せず
+//   ポインタのコピーが発生します。
+//   sequence のサイズは sizeof(int) * (1 + SIZE) ですので
+//   大抵の処理系では 4004 bytes です。それに対してポインタは
+//   8 bytes と圧倒的に小さいです。
 void insert(sequence* seq, int pos, int val) {
   if (seq->length >= SIZE) {
     printf("No more element can be inserted into elements.\n");
