6c2a993fbe311f34b59662ed14951bfdc8214a7a
Yuki Nishidate <asta18425@gmail.com>
2022-09-26 20:11:30 +0900

    refactor

--- 06/binary_search.c
+++ 06/binary_search.c
@@ -3,308 +3,151 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * @brief 文字列化.
- */
-#define STRINGFY(s) #s
-/**
- * @brief 定義された値に変化しSTRINGFY.
- */
-#define DEF_STRINGFY(def) STRINGFY(def)
+#define MAX_NUM_RECORDS 1000
+#define MAX_VALUE_SIZE 32
+#define SAMPLE_RECORDS 5
 
-/**
- * @brief 配列がとれる最大バイト数.
- */
-#define MAX_ARRAY_MEMORY 65535
-/**
- * @brief レコードのフィールドのデータが占有できる最大バイト数.
- */
-#define MAX_FIELD_MEMORY 32
-/**
- * @brief サンプルで使用するrecordsの量
- */
-#define INITIAL_NUM_RECORDS 10
-
-/**
- * @brief レコード：keyとfieldをもった構造体
- */
-typedef struct record {
-  int key;                      /** int型のキー. key==-1or2^64-1を例外処理に用いている*/
-  char field[MAX_FIELD_MEMORY]; /** データを保持するchar型の配列. */
+typedef struct {
+  int key;
+  char value[MAX_VALUE_SIZE];
 } record;
 
-/**
- * @brief 表：recordの配列とそのメタデータを保持する構造体.
- */
-typedef struct table {
-  int records_length;                                /** 配列の長さ. */
-  int size;                                          /** 配列がとれる最大長. */
-  record records[MAX_ARRAY_MEMORY / sizeof(record)]; /** record型で表された配列. */
+typedef struct {
+  int length;
+  record records[MAX_NUM_RECORDS];
 } table;
 
-/**
- * @brief recordの初期化.
- * @param[in] key 指定するキー.
- * @param[in] field const char*,動的にするのであればchar*,サイズはMAX_FIELD_MEMORYで定義.
- * @return 初期化されたrecordのポインタ.
- */
-record *init_record(int key, const char *field) {
-  if (strlen(field) > MAX_FIELD_MEMORY + 1) {
-    fprintf(stderr, "ERROR: \"field\" is too large.\n");
-    exit(1);
+void swap(int* a, int* b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+  return;
+}
+
+// NOTE: Fisher–Yates shuffle というアルゴリズムを使います
+void shuffle(int* array, int length) {
+  int i = length;
+  while (i > 1) {
+    int j = rand() % i--;
+    swap(&array[i], &array[j]);
   }
-
-  record *rec = (record *)malloc(sizeof(record));
-  rec->key = key;
-  strncpy(rec->field, field, MAX_FIELD_MEMORY);
-
-  return rec;
 }
 
-/**
- * @brief tableの初期化.
- * @return 初期化されたtableのポインタ.
- */
-table *init_table() {
-  table *tab = (table *)malloc(sizeof(table));
-  tab->records_length = 0;
-  tab->size = MAX_ARRAY_MEMORY / sizeof(record);
-  return tab;
-}
+typedef struct {
+  bool found;
+  int index;
+} result;
 
-/**
- * @brief tableのメモリを解放.
- * @param[in] tab メモリを開放するtable.
- */
-void release_table(table *tab) {
-  free(tab);
-  tab = NULL;
-}
-
-/**
- * @brief tableのrecordsにtargetが存在するか調べ,その時による真理値とインデックスを返す.
- * @param[in] tab targetが存在するか調べるtable.
- * @param[in] target 調べるキー.
- * @param[out] found targetがtab内に存在するかの真理値.
- * @param[out] index targetが存在する場合キーのインデックスを指す,無い場合は-1==2^64-1.
- */
-void linear_search_existence_and_index(table *tab, int target, bool *found, int *index) {
-  tab->records[tab->records_length].key = target;
-  int searching_pos = 0;
-  while (target != tab->records[searching_pos].key) {
-    searching_pos++;
-  }
-  *found = searching_pos < tab->records_length;
-  *index = *found ? searching_pos : -1;
-}
-
-/**
- * @brief 二分探索でtableのrecordsにtargetが存在するか調べ,その時による真理値とインデックスを返す.
- * @param[in] tab targetが存在するか調べるtable,整列になっている.
- * @param[in] target 調べるキー.
- * @param[out] found targetがtab内に存在するかの真理値.
- * @param[out] index targetが存在する場合キーのインデックスを指す,無い場合はtarget以上の最小キー.
- */
-void binary_search_existence_and_index(table *tab, int target, bool *found, int *index) {
-  long int lo = 0, hi = tab->records_length - 1, mid;
-  while (lo <= hi) {
-    // whileから出るワンステップ前になりえる状態はlo==hiまたはlo==hi+1(lo==mid+1で前者になる).
-    // target <= tab->records[mid].keyによって,whileから抜けた後,
-    // hiはtarget未満の最大を指すインデックス.
-    // loはtarget以上の最小を指すインデックス.
-    mid = (lo + hi) / 2;
-    if (target <= tab->records[mid].key) {
-      hi = mid - 1;
+// NOTE: key 以上となる値が現れる最小の index を返します
+// もし index が tab->length であれば条件を満たす値が存在しません
+// この仕様は C++ の std::lower_bound に準拠しています
+int search(table* tab, int target) {
+  int low = 0;
+  int high = tab->length - 1;
+  while (low <= high) {
+    int middle = (low + high) / 2;
+    if (target <= tab->records[middle].key) {
+      high = middle - 1;
     } else {
-      lo = mid + 1;
+      low = middle + 1;
     }
   }
-  //削除していない数字が合った場合をケア
-  *found = lo == (long int)tab->records_length ? false : target == tab->records[lo].key;
-  *index = lo;
-
-  //二分探索アルゴリズムを一般化 〜 めぐる式二分探索法のススメ 〜
-  // https://qiita.com/drken/items/97e37dd6143e33a64c8c
-  // 上記と異なり
-  // okは常に条件を満たすインデックス
-  // ngは常に条件を満たさないインデックス
-  /*
-  int ng = -1;
-  int ok = tab->records_length; // tab->records[tab->records_length - 1].key < targetのことを考えて
-  int mid;
-  while (ok - ng > 1)
-  {
-    mid = (ok + ng) / 2;
-    if (target <= tab->records[mid].key)
-    {
-      ok = mid;
-    }
-    else
-    {
-      ng = mid;
-    }
-  }
-
-  *found = ok == tab->records_length ? false : target == tab->records[ok].key;
-  *index = ok;
-  */
+  return low;
 }
 
-/**
- * @brief tableのrecordsの末尾にデータを挿入する.
- * @param[in] tab 挿入対象のrecordsをもつtableのポインタ.
- * @param[in] rec 挿入するrecord.
- */
-void insert_record(table *tab, record *rec) {
-  if (tab->records_length >= tab->size - 1) {
+void insert(table* tab, record rec) {
+  if (tab->length >= MAX_NUM_RECORDS - 1) {
     printf("ERROR: No more record can be inserted into table.\n");
     return;
   }
 
-  bool dummy;
-  int inserting_pos;
-  binary_search_existence_and_index(tab, rec->key, &dummy, &inserting_pos);
-
-  for (int i = tab->records_length; i > inserting_pos; i--) {
+  // 順序が正しく保たれるように、正しい挿入位置を計算します
+  int index = search(tab, rec.key);
+  for (int i = tab->length; i > index; i--) {
     tab->records[i] = tab->records[i - 1];
   }
 
-  tab->records[inserting_pos] = *rec;
-  tab->records_length++;
+  tab->records[index] = rec;
+  tab->length++;
 }
 
-/**
- * @brief tableのrecordsの位置posを削除する.
- * @param[in] tab 削除対象のrecordsをもつtableのポインタ.
- * @param[in] pos 削除するrecordsの位置.
- */
-void delete_record(table *tab, int pos) {
-  if (pos == (int)-1) {
-    printf("Valid pos was not set to delte. pos=\"%d\".\n", pos);
-    return;
-  }
-  for (int i = pos; i < tab->records_length - 1; i++) {
+void erase(table* tab, int pos) {
+  for (int i = pos; i < tab->length - 1; i++) {
     tab->records[i] = tab->records[i + 1];
   }
-  tab->records_length--;
+  tab->length--;
 }
 
-/**
- * @brief cliからrecordの内容を読み取りrecordに格納.
- * @return cliから読み取った内容で生成したrecordのポインタ
- */
-record *cli_record() {
-  record *rec;
-  int key = -1;
-  char field[MAX_FIELD_MEMORY];
-
-  printf("Type in a key >= 0 and a field. (example: \"10001 BBB\")\n");
-  printf(STRINGFY(MAX_FIELD_MEMORY) "=" DEF_STRINGFY(MAX_FIELD_MEMORY) "\n");
-
-  while (key == -1) {
-    scanf("%d", &key);
-  }
-  scanf("%" DEF_STRINGFY(MAX_FIELD_MEMORY) "s%*[^\n]", field);
-
-  rec = init_record(key, field);
-
-  return rec;
-}
-
-/**
- * @brief cliからrecordの内容を読み取り,tableの末尾に挿入.
- * @param[in] tab 挿入対象のrecordsをもつtableのポインタ.
- */
-void cli_insert(table *tab) {
-  printf("ENETER A RECORD THAT WILL BE INSERTED.\n");
-  record *scanned_rec = NULL;
-  bool does_scanned_rec_exist = false;
+void cli_insert(table* tab) {
+  printf("Type in a key (>= 0) and a field. (example: \"100 BBB\")\n");
   while (true) {
-    scanned_rec = cli_record();
-    int dummy;
-    binary_search_existence_and_index(tab, scanned_rec->key, &does_scanned_rec_exist, &dummy);
-    if (does_scanned_rec_exist) {
+    record rec;
+    scanf("%d %s", &rec.key, rec.value);
+    if (search(tab, rec.key) != tab->length) {
       printf("The key is already used.\n");
     } else {
-      break;
+      insert(tab, rec);
+      return;
     }
   }
-  insert_record(tab, scanned_rec);
 }
 
-/**
- * @brief record確認用プリント関数.
- * @param[in] rec プリントするrecordのポインタ.
- */
-void print_record(record *rec) {
-  printf("%d, \"%s\"\n", rec->key, rec->field);
-}
-
-/**
- * @brief table確認用プリント関数.
- * @param[in] tab プリントする配列をもつtableのポインタ.
- */
-void print_table(table *tab) {
-  printf("================================\n");
-  printf("TABLE: [\n");
-  for (int i = 0; i < tab->records_length; i++) {
-    print_record(&tab->records[i]);
+void print(table* tab) {
+  printf("TABLE: [ ");
+  for (int i = 0; i < tab->length; i++) {
+    printf("{%d, %s} ", tab->records[i].key, tab->records[i].value);
   }
-  printf("]\nTABLE LENGTH: %d\n", tab->records_length);
-  printf("================================\n");
-}
-
-/**
- * @brief binary_search_existence_and_index確認用プリント関数.
- * @param[in] tab targetが存在するか調べるtable.
- * @param[in] target 調べるキー.
- */
-void print_search_existence(table *tab, int target) {
-  bool b = false;
-  int dummy;
-  binary_search_existence_and_index(tab, target, &b, &dummy);
-  printf("\"%d\" was %s\n", target, b ? "FOUND." : "NOT FOUND.");
+  printf("]\n");
+  printf("LENGTH: %d\n\n", tab->length);
 }
 
 int main() {
-  table *tab = init_table();
-  record *rec = NULL;
-  for (int i = 0; i < INITIAL_NUM_RECORDS; i++) {
-    rec = init_record(i, "AAA");
-    insert_record(tab, rec);
+  table tab = {0};
+  for (int i = 0; i < SAMPLE_RECORDS; i++) {
+    record rec = {i, "AAA"};
+    insert(&tab, rec);
   }
-  print_table(tab);
-  print_search_existence(tab, 5);
+  print(&tab);
 
-  // cli_insert(tab);
-  // print_table(tab);
+  // insert user input
+  cli_insert(&tab);
+  print(&tab);
 
-  bool dummy;
-  int target_pos;
-  binary_search_existence_and_index(tab, 5, &dummy, &target_pos);
-  delete_record(tab, target_pos);
+  // search 3
+  int target = 3;
+  int index = search(&tab, target);
+  if (index != tab.length && tab.records[index].key == target) {
+    printf("%d was %s\n", target, tab.records[index].value);
+  } else {
+    printf("%d was NOT FOUND.\n", target);
+  }
 
-  print_table(tab);
-  print_search_existence(tab, 5);
+  // erase 3
+  erase(&tab, index);
+  print(&tab);
 
-  insert_record(tab, init_record(5, "TEST"));
-  print_table(tab);
+  // search 3
+  index = search(&tab, target);
+  if (index != tab.length && tab.records[index].key == target) {
+    printf("%d was %s\n", target, tab.records[index].value);
+  } else {
+    printf("%d was NOT FOUND.\n", target);
+  }
 
-  release_table(tab);
   return 0;
 }
 
-/*
-void insert_tail(table *tab, record *rec)
-{
-  if (tab->records_length >= tab->size - 1)
-  {
-    printf("ERROR: No more record can be inserted into table.\n");
-    return;
-  }
-
-  tab->records[tab->records_length] = *rec;
-  tab->records_length++;
-  //下に整列させるアルゴリズムがなければ二分探索の前提が崩れる.
-}
-*/
+// 実行結果
+// TABLE: [ {0, AAA} {1, AAA} {2, AAA} {3, AAA} {4, AAA} ]
+// LENGTH: 5
+//
+// Type in a key (>= 0) and a field. (example: "100 BBB")
+// 5 B
+// TABLE: [ {0, AAA} {1, AAA} {2, AAA} {3, AAA} {4, AAA} {5, B} ]
+// LENGTH: 6
+//
+// 3 was AAA
+// TABLE: [ {0, AAA} {1, AAA} {2, AAA} {4, AAA} {5, B} ]
+// LENGTH: 5
+//
+// 3 was NOT FOUND.
