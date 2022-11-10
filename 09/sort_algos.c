ee61c98e84beb534326c1c2ab8990652ac04c8d6
nishidate-yuki <yuki18425@gmail.com>
2022-11-11 02:33:54 +0900

    refactor

--- 09/sort_algos.c
+++ 09/sort_algos.c
@@ -1,306 +1,107 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define MAX_RECORD_SIZE 10
+// 時間計測をする際には大きな数値にしてください。
+#define SIZE 10
 
-#define SWAP(type, a, b)   \
-    {                      \
-        type swap_tmp = a; \
-        a = b;             \
-        b = swap_tmp;      \
-    }
-
-typedef struct {
-    int key;
-    char field[32];
-} record;
-
-typedef struct {
-    int elements_length;
-    int size;
-    record *elements[MAX_RECORD_SIZE];
-} sequence;
-
-record *init_record(int key, char *field) {
-    record *rec = (record *)malloc(sizeof(record));
-    if (rec == NULL) {
-        fprintf(stderr, "ERROR: Not enough memory for new record.\n");
-        exit(1);
-    }
-
-    rec->key = key;
-    strcpy(rec->field, field);
-
-    return rec;
+void swap(int* a, int* b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-sequence *init_sequence() {
-    sequence *s = (sequence *)malloc(sizeof(sequence));
-    s->elements_length = 0;
-    s->size = MAX_RECORD_SIZE;
-    return s;
-}
-
-void release_record(record **rec) {
-    free(*rec);
-    *rec = NULL;
-}
-
-void release_sequence(sequence **seq) {
-    for (int i = 0; i < (*seq)->size; i++) {
-        if ((*seq)->elements[i] != NULL) {
-            release_record(&(*seq)->elements[i]);
-        }
-    }
-    free(*seq);
-    *seq = NULL;
-}
-
-bool is_record_a_larger_than_b(record *a, record *b) { return a->key > b->key; }
-
-bool is_a_larger_than_b(record **records, int a, int b) {
-    return is_record_a_larger_than_b(records[a], records[b]);
-}
-
-void compare_and_swap(record **records, int a, int b) {
-    if (is_a_larger_than_b(records, a, b)) {
-        SWAP(record *, records[a], records[b])
-    }
-}
-
-void insert_tail(sequence *seq, record *rec) {
-    if (seq->elements_length >= seq->size) {
-        printf("ERROR: No more record can be inserted into table.\n");
-        return;
-    }
-
-    if (seq->elements[seq->elements_length] == NULL) {
-        seq->elements[seq->elements_length] = rec;
-    } else {
-        release_record(&seq->elements[seq->elements_length]);
-        printf("The inserting index was dirty, so it was cleaned.\n");
-
-        seq->elements[seq->elements_length] = rec;
-    }
-    seq->elements_length++;
-}
-
-void binary_search_existence_and_index(sequence *seq, int target, bool *found,
-                                       int *index) {
-    int ng = -1;
-    int ok = seq->elements_length;
-    int mid;
-    while (ok - ng > 1) {
-        mid = (ok + ng) / 2;
-        if (target <= seq->elements[mid]->key) {
-            ok = mid;
-        } else {
-            ng = mid;
-        }
-    }
-
-    *found =
-        ok == seq->elements_length ? false : target == seq->elements[ok]->key;
-    *index = ok;
-}
-
-void delete_target_record(sequence *seq, int target) {
-    bool found;
-    int target_index;
-    binary_search_existence_and_index(seq, target, &found, &target_index);
-    if (found) {
-        release_record(&seq->elements[target_index]);
-        for (int i = target_index; i < seq->elements_length - 1; i++) {
-            seq->elements[i] = seq->elements[i + 1];
-        }
-        seq->elements[seq->elements_length - 1] = NULL;
-        seq->elements_length--;
-    } else {
-        printf("ERROR: Target was not found, and cannot be deleted.\n");
-    }
-}
-
-/**
- * @brief 単純なソート.
- * @param[in] seq ソートするsequenceのポインタ.
- */
-void simple_sort(sequence *seq) {
-    for (int i = 0; i < seq->elements_length - 1; i++) {
-        for (int j = i + 1; j < seq->elements_length; j++) {
-            compare_and_swap(seq->elements, i, j);
+void simple_sort(int* array) {
+    for (int i = 0; i < SIZE - 1; i++) {
+        for (int j = i + 1; j < SIZE; j++) {
+            if (array[i] > array[j]) {
+                swap(&array[i], &array[j]);
+            }
         }
     }
 }
 
-/**
- * @brief バブルソート.
- * @param[in] seq ソートするsequenceのポインタ.
- */
-void bubble_sort(sequence *seq) {
-    for (int i = 0; i < seq->elements_length - 1; i++) {
-        for (int j = seq->elements_length - 1; j > i; j--) {
-            compare_and_swap(seq->elements, j - 1, j);
+void bubble_sort(int* array) {
+    for (int i = 0; i < SIZE - 1; i++) {
+        for (int j = SIZE - 1; j > i; j--) {
+            if (array[j - 1] > array[j]) {
+                swap(&array[j - 1], &array[j]);
+            }
         }
     }
 }
 
-/**
- * @brief 選択ソート.
- * @param[in] seq ソートするsequenceのポインタ.
- */
-void selection_sort(sequence *seq) {
-    int min_pos;
-    for (int i = 0; i < seq->elements_length - 1; i++) {
-        min_pos = i;
-        for (int j = i + 1; j < seq->elements_length; j++) {
-            if (is_a_larger_than_b(seq->elements, min_pos, j)) {
+void selection_sort(int* array) {
+    for (int i = 0; i < SIZE - 1; i++) {
+        int min_pos = i;
+        for (int j = i + 1; j < SIZE; j++) {
+            if (array[j] < array[min_pos]) {
                 min_pos = j;
             }
         }
-        SWAP(record *, seq->elements[i], seq->elements[min_pos])
+        swap(&array[i], &array[min_pos]);
     }
 }
 
-/**
- * @brief 挿入ソート.
- * @param[in] seq ソートするsequenceのポインタ.
- */
-void insertion_sort(sequence *seq) {
-    record *inserting_record;
-    int j = 0;
-
-    for (int i = 1; i < seq->elements_length; i++) {
-        inserting_record = seq->elements[i];
-        /**
-         * long unsigned int / intのためj>=0をj!=-1に
-         * 番兵にしていないため,jに関する条件式が必要.
-         * 番兵にしたければ:
-         * 1.挿入などを1オリジンにしてインデックス0を番兵に使えるようにする.
-         * 2.末尾を番兵として,符号付きintにして-1のmodularをとって配列の末尾のインデックスをとる.
-         * 3.全部を一個後ろにシフトする.
-         * また番兵を用いる事を考えるならば,
-         * MAX_RECORD_SIZEの定義に+1をし,番兵分をつくる.
-         * */
-        for (j = i - 1; j != (int)-1; j--) {
-            /** 挿入するrecord->key以上かつ最小のインデックスを探すまで,後ろにシフト*/
-            if (is_record_a_larger_than_b(seq->elements[j], inserting_record)) {
-                seq->elements[j + 1] = seq->elements[j];
-                continue;
-            } else {
-                break;
-            }
+void insertion_sort(int* array) {
+    for (int i = 1; i < SIZE; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (array[j] <= array[j + 1]) break;
+            swap(&array[j], &array[j + 1]);
         }
-        seq->elements[j + 1] = inserting_record;
     }
 }
 
-/**
- * @brief record確認用プリント関数.
- * @param[in] rec プリントするrecordのポインタ.
- */
-void print_record(record *rec) {
-    if (rec == NULL) {
-        printf("ERROR: Record is NULL\n");
-    } else {
-        printf("[%d, %s]\n", rec->key, rec->field);
+void print(int* array) {
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", array[i]);
     }
-}
-
-/**
- * @brief sequence確認用プリント関数.
- * @param[in] seq プリントするsequenceのポインタ.
- */
-void print_sequence(sequence *seq) {
-    if (seq == NULL) {
-        printf("ERROR: Sequence is NULL\n");
-    } else {
-        printf("\n");
-        printf("================================\n");
-        printf("Printing sequence\n");
-        printf("================================\n");
-        for (int i = 0; i < seq->elements_length; i++) {
-            print_record(seq->elements[i]);
-        }
-        printf("================================\n");
-        printf("Sequence size: (%d/%d)\n", seq->elements_length, seq->size);
-        printf("================================\n");
-        printf("\n");
-    }
-}
-
-/**
- * @brief binary_search_existence_and_index確認用プリント関数.
- * @param[in] tab targetが存在するか調べるsequence.
- * @param[in] target 調べるキー.
- */
-void print_search_existence_and_index(sequence *seq, int target) {
-    printf("\n");
-    printf("================================\n");
-
-    bool found = false;
-    int target_index;
-    binary_search_existence_and_index(seq, target, &found, &target_index);
-    printf("\"%d\" was %s\n", target, found ? "FOUND." : "NOT FOUND.");
-    if (found) {
-        printf("The content of the target is...\n");
-        print_record(seq->elements[target_index]);
-    }
-    printf("================================\n");
     printf("\n");
 }
 
-void run_sort_algorithm(sequence *seq, void (*sort_algorithm)(sequence *)) {
+void run_sort_algorithm(int* array, void (*sort_algorithm)(int*)) {
     double start_clock, end_clock;
-
     start_clock = (double)clock();
-    sort_algorithm(seq);
+    sort_algorithm(array);
     end_clock = (double)clock();
-
-    printf("CPU execution time: %.8lf s.\n",
-           (end_clock - start_clock) / CLOCKS_PER_SEC);
+    printf("Time  : %.8lf s\n", (end_clock - start_clock) / CLOCKS_PER_SEC);
 }
 
-void fisher_yates_shuffle(int *array, int array_size) {
-    int i = array_size;
+void shuffle(int* array, int length) {
+    int i = length;
     while (i > 1) {
-        int j = rand() % i;
-        i--;
-        int t = array[i];
-        array[i] = array[j];
-        array[j] = t;
+        int j = rand() % i--;
+        swap(&array[i], &array[j]);
     }
 }
 
 int main() {
-    int keys[MAX_RECORD_SIZE];
-    int num_keys = sizeof(keys) / sizeof(int);
-    for (int i = 0; i < MAX_RECORD_SIZE; i++) {
+    int keys[SIZE];
+    for (int i = 0; i < SIZE; i++) {
         keys[i] = i;
     }
+    shuffle(keys, SIZE);
 
-    fisher_yates_shuffle(keys, num_keys);
-
-    sequence *seq = init_sequence();
-    for (int i = 0; i < MAX_RECORD_SIZE; i++) {
-        insert_tail(seq, init_record(keys[i], (char *)"AAAAAAAA"));
+    int* array = (int*)malloc(sizeof(int) * SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        array[i] = keys[i];
     }
 
-    print_sequence(seq);
-    printf("Using algorithm: bubble_sort\n");
-    run_sort_algorithm(seq, bubble_sort);
+    printf("Before: ");
+    print(array);
+    run_sort_algorithm(array, simple_sort);
+    // run_sort_algorithm(array, bubble_sort);
+    // run_sort_algorithm(array, insertion_sort);
+    printf("After : ");
+    print(array);
 
-    print_sequence(seq);
-    print_search_existence_and_index(seq, 25);
-
-    delete_target_record(seq, 25);
-
-    print_sequence(seq);
-    print_search_existence_and_index(seq, 25);
-
-    release_sequence(&seq);
-    print_sequence(seq);
     return 0;
 }
+
+// 実行結果
+// Before: 9 8 4 2 0 6 5 1 7 3
+// Time  : 0.00000100 s
+// After : 0 1 2 3 4 5 6 7 8 9
