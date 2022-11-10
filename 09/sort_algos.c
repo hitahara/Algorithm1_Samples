f25cbbbc66e8b65797ac2673e8274d11b9b558ab
nishidate-yuki <yuki18425@gmail.com>
2022-11-11 01:35:14 +0900

    remove some code

--- 09/sort_algos.c
+++ 09/sort_algos.c
@@ -13,62 +13,17 @@
         b = swap_tmp;      \
     }
 
-typedef struct record record;
-typedef struct sequence sequence;
+typedef struct {
+    int key;
+    char field[32];
+} record;
 
-record *init_record(int key, char *field);
-sequence *init_sequence();
-void release_record(record **rec);
-void release_sequence(sequence **seq);
+typedef struct {
+    int elements_length;
+    int size;
+    record *elements[MAX_RECORD_SIZE];
+} sequence;
 
-bool is_record_a_larger_than_b(record *a, record *b);
-bool is_a_larger_than_b(record **records, int a, int b);
-void compare_and_swap(record **records, int a, int b);
-
-void insert_tail(sequence *seq, record *rec);
-
-void binary_search_existence_and_index(sequence *seq, int target, bool *found,
-                                       int *index);
-
-void simple_sort(sequence *seq);
-void bubble_sort(sequence *seq);
-void selection_sort(sequence *seq);
-void insertion_sort(sequence *seq);
-
-void print_record(record *rec);
-void print_sequence(sequence *seq);
-void print_search_existence_and_index(sequence *seq, int target);
-
-record *cli_record();
-void cli_insert(sequence *seq);
-void run_sort_algorithm(sequence *seq, void (*sort_algorithm)(sequence *));
-
-void fisher_yates_shuffle(int *array, int array_size);
-
-/**
- * @brief レコード：keyとfieldをもった構造体
- */
-struct record {
-    int key; /** int型のキー. key==-1or2^64-1を例外処理に用いている*/
-    char field[32]; /** データを保持するchar型の配列. */
-};
-
-/**
- * @brief 配列とそのメタデータを保持する構造体.
- */
-struct sequence {
-    int elements_length;               /** 配列の長さ. */
-    int size;                          /** 配列がとれる最大長. */
-    record *elements[MAX_RECORD_SIZE]; /** recordのポインタの配列. */
-};
-
-/**
- * @brief recordの初期化.
- * @param[in] key 初期化時に指定するキー.
- * @param[in] field 静的ならばconst char*,動的ならばchar*にしなければいけない,
- * サイズはMAX_FIELD_SIZEで定義.初期化時に指定するフィールド.
- * @return 初期化されたrecordのポインタ.
- */
 record *init_record(int key, char *field) {
     record *rec = (record *)malloc(sizeof(record));
     if (rec == NULL) {
@@ -81,10 +36,7 @@ record *init_record(int key, char *field) {
 
     return rec;
 }
-/**
- * @brief sequenceの初期化.
- * @return 初期化されたsequenceのポインタ.
- */
+
 sequence *init_sequence() {
     sequence *s = (sequence *)malloc(sizeof(sequence));
     s->elements_length = 0;
@@ -92,21 +44,12 @@ sequence *init_sequence() {
     return s;
 }
 
-/**
- * @brief recordのメモリを解放.
- * @param[in] rec メモリを開放するrecord.
- */
 void release_record(record **rec) {
     free(*rec);
     *rec = NULL;
 }
 
-/**
- * @brief sequenceのメモリを解放.
- * @param[in] seq メモリを開放するsequence.
- */
 void release_sequence(sequence **seq) {
-    // seq内のmallocで生成されたrecord群のメモリも解放.
     for (int i = 0; i < (*seq)->size; i++) {
         if ((*seq)->elements[i] != NULL) {
             release_record(&(*seq)->elements[i]);
@@ -116,42 +59,18 @@ void release_sequence(sequence **seq) {
     *seq = NULL;
 }
 
-/**
- * @brief 2つのreocrdをkeyで評価.
- * @param[in] a 評価するrecord.
- * @param[in] b 評価するrecord.
- * @return a->key > b->keyの場合:true,a->key > b->keyの場合:false.
- */
 bool is_record_a_larger_than_b(record *a, record *b) { return a->key > b->key; }
 
-/**
- * @brief 2つのインデックスでのkeyを評価.
- * @param[in] records 評価するインデックスが指すrecordのポインタの配列.
- * @param[in] a 評価するインデックスのなかで最小(a<b).
- * @param[in] b 評価するインデックスのなかで最大(a<b).
- * @return a>bの場合:true,a<bの場合:false.
- */
 bool is_a_larger_than_b(record **records, int a, int b) {
     return is_record_a_larger_than_b(records[a], records[b]);
 }
 
-/**
- * @brief 2つのインデックスでのkeyを評価し,[a]>[b]の場合スワップを決定する.
- * @param[in] records 評価するインデックスが指すrecordのポインタの配列.
- * @param[in] a 評価するインデックスのなかで最小(a<b).
- * @param[in] b 評価するインデックスのなかで最大(a<b).
- */
 void compare_and_swap(record **records, int a, int b) {
     if (is_a_larger_than_b(records, a, b)) {
         SWAP(record *, records[a], records[b])
     }
 }
 
-/**
- * @brief sequenceの末尾にデータを挿入する.
- * @param[in] seq recを挿入するsequenceのポインタ.
- * @param[in] rec 挿入するrecordのポインタ.
- */
 void insert_tail(sequence *seq, record *rec) {
     if (seq->elements_length >= seq->size) {
         printf("ERROR: No more record can be inserted into table.\n");
@@ -169,35 +88,6 @@ void insert_tail(sequence *seq, record *rec) {
     seq->elements_length++;
 }
 
-/**
- * @brief sequenceのkey==targetを削除する.
- * @param[in] seq key==targetを削除するsequenceのポインタ.
- * @param[in] target 削除するキー.
- */
-void delete_target_record(sequence *seq, int target) {
-    bool found;
-    int target_index;
-    binary_search_existence_and_index(seq, target, &found, &target_index);
-    if (found) {
-        release_record(&seq->elements[target_index]);
-        for (int i = target_index; i < seq->elements_length - 1; i++) {
-            seq->elements[i] = seq->elements[i + 1];
-        }
-        seq->elements[seq->elements_length - 1] = NULL;
-        seq->elements_length--;
-    } else {
-        printf("ERROR: Target was not found, and cannot be deleted.\n");
-    }
-}
-
-/**
- * @brief
- * 二分探索でsequenceにtargetが存在するか調べ,その時による真理値とインデックスを返す.
- * @param[in] tab targetが存在するか調べるsequence,整列になっている.
- * @param[in] target 調べるキー.
- * @param[out] found targetがtab内に存在するかの真理値.
- * @param[out] index sequenceのなかで,target以上であり最小のキー.
- */
 void binary_search_existence_and_index(sequence *seq, int target, bool *found,
                                        int *index) {
     int ng = -1;
@@ -215,6 +105,22 @@ void binary_search_existence_and_index(sequence *seq, int target, bool *found,
     *found =
         ok == seq->elements_length ? false : target == seq->elements[ok]->key;
     *index = ok;
+}
+
+void delete_target_record(sequence *seq, int target) {
+    bool found;
+    int target_index;
+    binary_search_existence_and_index(seq, target, &found, &target_index);
+    if (found) {
+        release_record(&seq->elements[target_index]);
+        for (int i = target_index; i < seq->elements_length - 1; i++) {
+            seq->elements[i] = seq->elements[i + 1];
+        }
+        seq->elements[seq->elements_length - 1] = NULL;
+        seq->elements_length--;
+    } else {
+        printf("ERROR: Target was not found, and cannot be deleted.\n");
+    }
 }
 
 /**
@@ -357,13 +263,25 @@ void run_sort_algorithm(sequence *seq, void (*sort_algorithm)(sequence *)) {
            (end_clock - start_clock) / CLOCKS_PER_SEC);
 }
 
+void fisher_yates_shuffle(int *array, int array_size) {
+    int i = array_size;
+    while (i > 1) {
+        int j = rand() % i;
+        i--;
+        int t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+}
+
 int main() {
     int keys[MAX_RECORD_SIZE];
+    int num_keys = sizeof(keys) / sizeof(int);
     for (int i = 0; i < MAX_RECORD_SIZE; i++) {
         keys[i] = i;
     }
 
-    fisher_yates_shuffle(keys, sizeof(keys) / sizeof(int));
+    fisher_yates_shuffle(keys, num_keys);
 
     sequence *seq = init_sequence();
     for (int i = 0; i < MAX_RECORD_SIZE; i++) {
@@ -385,20 +303,4 @@ int main() {
     release_sequence(&seq);
     print_sequence(seq);
     return 0;
-}
-
-/**
- * @brief 指定されたintのポインタにある配列にfiesher-yates shuffle.
- * @param[in,out] array シャフルする配列であるintのポインタ.
- * @param[in] array_size シャッフルする配列のサイズ
- */
-void fisher_yates_shuffle(int *array, int array_size) {
-    int i = array_size;
-    while (i > 1) {
-        int j = rand() % i;
-        i--;
-        int t = array[i];
-        array[i] = array[j];
-        array[j] = t;
-    }
 }
